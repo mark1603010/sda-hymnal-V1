f6f3ea9a72b2743bd33d32508d7480f7c689ecfd
mark1603010 <61652210+mark1603010@users.noreply.github.com>
2025-09-28 14:37:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -291,8 +291,8 @@ function enableNoteZoom() {
   noteImage.dataset.zoomEnabled = 'true';
 }
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js', { scope: './' })
-    .then(reg => console.log('Service Worker registered:', reg))
-    .catch(err => console.error('Service Worker registration failed:', err));
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('sw.js', { scope: './' })
+//     .then(reg => console.log('Service Worker registered:', reg))
+//     .catch(err => console.error('Service Worker registration failed:', err));
+// }
