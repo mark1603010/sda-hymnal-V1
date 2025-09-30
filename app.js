99e9ed728dc532a9bddedb268d3aef1c91e9b511
mark1603010 <61652210+mark1603010@users.noreply.github.com>
2025-09-30 21:58:24 +0800

    new

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const darkModeToggle = document.getElementById('darkModeToggle');
 let hymns = [];
 let currentFontSize = 1.7;
 
-const CURRENT_VERSION = 'v1.0.0'; // Match your CACHE_NAME in sw.js
+const CURRENT_VERSION = 'v1.0.1'; // Match your CACHE_NAME in sw.js
 console.log('Hymnal App running version:', CURRENT_VERSION);
 
 
